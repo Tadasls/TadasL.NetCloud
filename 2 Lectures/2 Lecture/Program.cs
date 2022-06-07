8873d4868f401a4b214ea94ea4a544d886100fa9
Tadas Laurinaitis <tadas.laurinaitis@gmail.com>
2022-06-07 18:12:26 +0300

    last

--- 2 Lectures/2 Lecture/Program.cs
+++ 2 Lectures/2 Lecture/Program.cs
@@ -66,7 +66,7 @@
             
 
             //1
-            Console.WriteLine(" Sveiki Tadai... Follow white rabbit,  Enter");
+            Console.WriteLine(" Sveiki Tadai... Follow white rabbit, Enter ") ;
 
             //2
             Console.WriteLine(" Iveskite savo varda, ir paspauskite Enter" + Console.ReadLine());
@@ -107,6 +107,60 @@
 
             //5C
             Console.WriteLine(" \n {0} \n {1} \n {2}", "Pirkti", "Parduoti", "Likuciai");
+
+
+            Console.WriteLine("--------");
+            Console.WriteLine("Standard DateTime Format Specifiers");
+            Console.WriteLine(
+                "(d) Short date: . . . . . . . {0:d}\n",
+
+                DateTime.Now);
+
+
+
+
+
+
+            /*
+            Console.WriteLine(
+        "(C) Currency: . . . . . . . . {0:C}\n" +
+        "(D) Decimal:. . . . . . . . . {0:D}\n" +
+        "(E) Scientific: . . . . . . . {1:E}\n" +
+        "(F) Fixed point:. . . . . . . {1:F}\n" +
+        "(G) General:. . . . . . . . . {0:G}\n" +
+        "    (default):. . . . . . . . {0} (default = 'G')\n" +
+        "(N) Number: . . . . . . . . . {0:N}\n" +
+        "(P) Percent:. . . . . . . . . {1:P}\n" +
+        "(R) Round-trip: . . . . . . . {1:R}\n" +
+        "(X) Hexadecimal:. . . . . . . {0:X}\n",
+        -123, -123.45f);
+
+
+            Console.WriteLine("--------");
+            Console.WriteLine("Standard DateTime Format Specifiers");
+            Console.WriteLine(
+                "(d) Short date: . . . . . . . {0:d}\n" +
+                "(D) Long date:. . . . . . . . {0:D}\n" +
+                "(t) Short time: . . . . . . . {0:t}\n" +
+                "(T) Long time:. . . . . . . . {0:T}\n" +
+                "(f) Full date/short time: . . {0:f}\n" +
+                "(F) Full date/long time:. . . {0:F}\n" +
+                "(g) General date/short time:. {0:g}\n" +
+                "(G) General date/long time: . {0:G}\n" +
+                "    (default):. . . . . . . . {0} (default = 'G')\n" +
+                "(M) Month:. . . . . . . . . . {0:M}\n" +
+                "(R) RFC1123:. . . . . . . . . {0:R}\n" +
+                "(s) Sortable: . . . . . . . . {0:s}\n" +
+                "(u) Universal sortable: . . . {0:u} (invariant)\n" +
+                "(U) Universal full date/time: {0:U}\n" +
+                "(Y) Year: . . . . . . . . . . {0:Y}\n",
+                DateTime.Now);
+
+            */
+
+
+
+
 
 
         }
