96422763887cfd4db4fb35584886950690644ba7
Tadas Laurinaitis <tadas.laurinaitis@gmail.com>
2022-06-07 18:36:34 +0300

    3

---
+++ 2 Lectures/Paskaita 2 consoles uzduotys/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Paskaita_2_consoles_uzduotys
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
