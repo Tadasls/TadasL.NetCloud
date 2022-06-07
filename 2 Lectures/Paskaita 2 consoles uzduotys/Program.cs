324d5af25a3fc10e8e189b301946390f3eac3cda
Tadas Laurinaitis <tadas.laurinaitis@gmail.com>
2022-06-07 19:39:49 +0300

    su navarotais

--- 2 Lectures/Paskaita 2 consoles uzduotys/Program.cs
+++ 2 Lectures/Paskaita 2 consoles uzduotys/Program.cs
@@ -4,7 +4,68 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //  Console.WriteLine("Tadas!");
+            //  Console.WriteLine("iveskime varda" + Console.ReadLine());
+            // Console.WriteLine ("    iveskime varda" + (int)Console.ReadKey().KeyChar);
+
+
+            /*
+            //nuskaityti pirma raide
+            Console.WriteLine(" Iveskime savo vardo pirma Raide");
+            //isvedimas i ekrana skaitymas is klaviaturos skaitymas antra karta informacijoss kastinimas matematikos suma ir kompociizija
+            Console.WriteLine("   Rezultatas yra {0}", (int)Console.ReadKey().KeyChar + (int)Console.ReadKey().KeyChar-48);
+            */
+
+            //Kontatinacija
+
+            /*
+
+            Console.WriteLine(" (1) Pirkti \n (2) Parduoti \n (3) Likuciai");
+            // Console.WriteLine("   Koks jusu pasirinkimas ?  \"   \"   : {0}", Console.ReadKey().KeyChar-48);
+             Console.WriteLine($"  \" pasirinkimas : {Console.ReadLine()} \" ");
+
+            */
+
+
+
+
+            /*
+            //labas su raides naujose eilutese
+             Console.WriteLine(" L\n A \n B \n A \n S ");
+            Console.WriteLine(" L  A   B    A     S ");
+            Console.WriteLine(@" 
+            L
+            A
+            B
+            A
+            S ");
+
+            Console.WriteLine("L");
+            Console.WriteLine("A");
+            Console.WriteLine("B");
+            Console.WriteLine("A");
+            Console.WriteLine("S");
+
+            */
+
+            /*
+            Console.WriteLine(" \" Labas \" ");
+
+            Console.WriteLine(" \u0022 Labas \u0022 ");
+            Console.WriteLine(" "+ (char)0x22 + "Labas\0022 ");
+            */
+
+
+
+            //Programa kuri nuskaito 
+            Console.WriteLine("Iveskita savo varda  " + Console.ReadLine()[1]); 
+           
+
+
+            
+
+
+
         }
     }
 }
