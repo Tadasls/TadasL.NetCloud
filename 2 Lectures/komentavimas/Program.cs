235dc2d94e66ccd2a81339bade4ca347c9c642f7
Tadas Laurinaitis <tadas.laurinaitis@gmail.com>
2022-06-07 18:34:10 +0300

    su kodavimu

---
+++ 2 Lectures/komentavimas/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace komentavimas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Komentarai******");
+            Console.WriteLine("Kokie buna komentarai ?");
+
+            //paprastas komentaras
+            /*
+             multiline komentaras
+            keliu eiluciu kometavimas
+
+             */
+
+            Console.WriteLine();  //komentaras uz kodo
+
+
+            var kintamasis = /* kazkoks komentaras */ "galima taip";
+
+
+                Console.WriteLine( "Kodel reikia rasyti komentarus?");
+
+            Console.WriteLine("Kai rasyti gerus komentarus?");
+
+
+            /*
+              reikia rasyti komentarus
+            kodo gabalui kuris yra funkciskai isbaigtas
+           kurie prideda prasmes
+             */
+
+
+            //blogas komentaras pvz.:
+
+            //Console Writeline naudoja isvesti i ekrana Hello word!
+            Console.WriteLine("Hello word!");
+
+
+
+
+            //Geras Komentaras
+            //isvesti hello word pirmoji mano veikianti programa
+            Console.WriteLine("Hello word!");
+
+
+        }
+    }
+}
