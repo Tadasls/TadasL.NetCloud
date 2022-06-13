3fa5e832f4fc90084863abf4035638836d915b66
Tadas Laurinaitis <tadas.laurinaitis@gmail.com>
2022-06-13 20:15:13 +0300

    updated in process

---
+++ 2 Lectures/P4 Matematikos operatoriai/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, Priskyrimo operatoriai = += -= *= /=!");
+
+int skaicius;
+int kitasSkaicius = 20; //pradzia 20 
+int nelyginisSkaicius = 5;
+
+skaicius = 10;
+kitasSkaicius = skaicius;    //kitasSkaicius tampa 10  
+Console.WriteLine($"skaiciusyra  = {skaicius}, o kitasSkaisius yra = {kitasSkaicius}  ");
+
+
+kitasSkaicius += nelyginisSkaicius;
+Console.WriteLine($"kitasSkaicius = {kitasSkaicius} ");  //rezultatas 15 t.y. 10 + 5
+
+kitasSkaicius -= nelyginisSkaicius;
+Console.WriteLine($"kitasSkaicius = {kitasSkaicius} ");  //rez is 15 - 5 gausis 10
+
+kitasSkaicius *= nelyginisSkaicius;
+Console.WriteLine($"kitasSkaicius = {kitasSkaicius} ");  //rez is 10 * 5 gausis 10
+
+kitasSkaicius = 21; // isivedeme reiksme kad pakeistume kad nesidalintu is 5 
+
+
+
+
+double doubleSkaicius = 21;
+    
+    
+doubleSkaicius /= (double)nelyginisSkaicius;
+Console.WriteLine($"kitasSkaicius patapes double = {doubleSkaicius} "); 
+
+
+
+
+
