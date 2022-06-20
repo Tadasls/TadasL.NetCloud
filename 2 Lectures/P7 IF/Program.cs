2b3a15a8f4746c8e9491a8ec0a711365768a8f5f
Tadas Laurinaitis <tadas.laurinaitis@gmail.com>
2022-06-20 22:00:52 +0300

    pirm pask

--- 2 Lectures/P7 IF/Program.cs
+++ 2 Lectures/P7 IF/Program.cs
@@ -6,6 +6,8 @@
         {
             Console.WriteLine("Hello, IF!");
 
+
+            /*
             int nelyginisSkaicius = 5;
             int lyginisSkaicius = 2;
             bool tiesa = true;
@@ -77,18 +79,16 @@
 
 
 
-            Console.WriteLine("-------------");
 
-            Console.WriteLine(" if kompozicija nesting");
+            Console.WriteLine("----------------------------");
+            Console.WriteLine("if kompozicija, nesting");
             int shields = 1, armor = 2;
-
-            // int shields = 1,
-            // int armor = 2;
-
+           // int shields = 1, taip irgi galima  kada tipai skiriasi
+           // int armor = 2;
 
             if (shields <= 0)
             {
-                if(armor <= 0)
+                if (armor <= 0)
                 {
                     Console.WriteLine("jus mires");
                 }
@@ -99,9 +99,108 @@
             }
             else
             {
-                Console.WriteLine("jus dar galite galimybiu");
+                Console.WriteLine("Jus dar turite galimybiu");
             }
 
+            if (shields <= 0 && armor <= 0)
+            {
+                Console.WriteLine("jus mires");
+            }
+            else if (shields <= 0 && armor > 0)
+            {
+                Console.WriteLine("jus dar turite armor");
+            }
+            else
+            {
+                Console.WriteLine("Jus dar turite galimybiu");
+            }
+
+            Console.WriteLine("================");
+            Console.WriteLine( " null-coalesccing operator (??)");
+
+            bool? nullableBool = true;
+            bool normalBool;
+            // normalBool = nullableBull;  // taip negalima
+
+            normalBool = nullableBool ?? false;
+            // -------
+            nullableBool ??= false;
+            
+
+                 Console.WriteLine("spuskite enter");
+            */
+            /*
+            //uzduotis 1
+
+            Console.WriteLine($"iveskite skaiciu",Console.ReadLine());
+            int ivestisA = Convert.ToInt32(Console.ReadLine());
+
+            if (ivestisA%2 == 0 )
+
+                {
+                Console.WriteLine("Skaicius lyginis");
+                }
+
+
+            if (ivestisA < 0)
+
+               {
+                Console.WriteLine("skaicius neigiamas");
+               } 
+
+            if (ivestisA % 2!= 0 && lyginisSkaicius >0)
+            {
+                Console.WriteLine("Skaisius" + ivestisA);
+            }
+
+            */
+
+            // uzduotis nr 2
+
+            /*
+            Console.WriteLine($"iveskite grupes nariu skaiciu");
+            int ivestisB = Convert.ToInt32(Console.ReadLine());
+
+            if (ivestisB == 1)
+                Console.WriteLine(" tai solo atlikejas");
+            else if (ivestisB == 2)
+                Console.WriteLine(" tai duetas");
+            else if (ivestisB > 2 && ivestisB < 10)
+                Console.WriteLine("tai ansamblis");
+            else if (ivestisB >= 10)
+                Console.WriteLine("tai kamerinis ansamblis");
+            else
+            {
+                Console.WriteLine("klaida");
+            }
+            */
+
+
+            // 3 uzduotis 
+        
+
+            Console.WriteLine($"iveskite isdirbtas valandas");
+            bool arGerasSkaicius = int.TryParse(Console.ReadLine(), out int input);
+            int imput;
+            if (arGerasSkaicius)
+    
+            if (input < 160)
+            {
+                Console.WriteLine($"dar reikia isdirbti  {160 - input} val");
+            } 
+            else if (input == 160) 
+            {
+                Console.WriteLine("isdirbtas Etapas");
+            }
+            else if (input > 160)
+            {
+                Console.WriteLine($"virsvalandziu  {input - 160 } val");
+            }
+            else
+            {
+                Console.WriteLine("klaida");
+            }
+           
 
 
 
@@ -111,12 +210,7 @@
 
 
 
-            Console.WriteLine("spuskite enter");
-                
-                
-                
-                
-                ;
+
         }
     }
 }
