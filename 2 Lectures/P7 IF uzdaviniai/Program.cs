08ffa2bdc72bfb810a15e924630e8bfd6eba8dc2
Tadas Laurinaitis <tadas.laurinaitis@gmail.com>
2022-06-21 17:51:23 +0300

    atr

--- 2 Lectures/P7 IF uzdaviniai/Program.cs
+++ 2 Lectures/P7 IF uzdaviniai/Program.cs
@@ -1,25 +1,24 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, If'ai!");
 
 //uzduotis 1
 
 Console.WriteLine($"iveskite skaiciu", Console.ReadLine());
 int ivestisA = Convert.ToInt32(Console.ReadLine());
 
-if (ivestisA % 2 == 0)
+if (ivestisA % 2 == 0) // skaiciuoja ar yra liekana dalinant is dvieju jei nelieka tai lyginis
 
 {
     Console.WriteLine("Skaicius lyginis");
 }
 
-
-if (ivestisA < 0)
+if (ivestisA < 0) // tikrina ar nera neigiamas
 
 {
     Console.WriteLine("skaicius neigiamas");
 }
 
-if (ivestisA % 2 != 0 && lyginisSkaicius > 0)
+if (ivestisA % 2 != 0 && ivestisA > 0) // tikrina ar lyginis ir ar teigiamas
 {
     Console.WriteLine("Skaisius" + ivestisA);
 }
@@ -30,7 +29,7 @@ if (ivestisA % 2 != 0 && lyginisSkaicius > 0)
 
 
 Console.WriteLine($"iveskite grupes nariu skaiciu");
-int ivestisB = Convert.ToInt32(Console.ReadLine());
+int ivestisB = Convert.ToInt32(Console.ReadLine());  // pavercia ivesti i skaicius
 
 if (ivestisB == 1)
     Console.WriteLine(" tai solo atlikejas");
@@ -51,8 +50,9 @@ else
 
 
 Console.WriteLine($"iveskite isdirbtas valandas");
-bool arGerasSkaicius = int.TryParse(Console.ReadLine(), out int input);
+bool arGerasSkaicius = int.TryParse(Console.ReadLine(), out int input); // kazkokia kieta funkcija
 int imput;
+
 if (arGerasSkaicius)
 
     if (input < 160)
