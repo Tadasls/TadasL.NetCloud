e230e5524a4ce3452550e2489605235952303208
Tadas Laurinaitis <tadas.laurinaitis@gmail.com>
2022-07-09 13:53:54 +0300

    tr

--- 2 Lectures/Belenkas1/Program.cs
+++ 2 Lectures/Belenkas1/Program.cs
@@ -9,116 +9,52 @@ namespace P014_Debug
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, Debug!");
-            Main2(null);
-        }
+            Console.WriteLine("Hello, ciklai!");
 
 
-        public static void Main2(string[] args)
-        {
-            Console.WriteLine("iveskite skaiciu a");
-            var a = Console.ReadLine();
-            Console.WriteLine("iveskite skaiciu b");
-            var b = Console.ReadLine();
-            Console.WriteLine(@"Pasirinkite veiksma
-            1) +
-            2) -
-            3) *
-            4) /
-            5) ^2
-            6) ^3
-            ");
-            var veiksmas = Console.ReadLine();
-
-            double? rezultatas = null;
-            rezultatas = Skaiciuotuvas(a, b, veiksmas);
-
-            Console.WriteLine($" {a} {veiksmas} {b} = {rezultatas}");
-
-        }
-
-        public static double? Skaiciuotuvas(string? a, string? b, string? veiksmas)
-        {
-            VeksmoNormalizacija(ref veiksmas);
-            if (ArSvekiejiSkaiciai(a, b) && !ArNaujasVeiksmas(veiksmas))
+            for (int i = 0; i <=5 ; i++)
             {
-                return Skaiciuotuvas(Convert.ToInt32(a), Convert.ToInt32(b), veiksmas);
+                Console.WriteLine(" reiksme i : {0}", i);
             }
-            else if (ArSkaiciai(a, b))
+            Console.WriteLine("-----------");
+            for (int i = 10-5; i >=0; i--)
             {
-                return Skaiciuotuvas(Convert.ToDouble(a), Convert.ToDouble(b), veiksmas);
+                Console.WriteLine(" reiksme i : {0}", i );
             }
+            Console.WriteLine("-----------");
 
-            return null;
-        }
-
-        public static void VeksmoNormalizacija(ref string veiksmas)
-        {
-            veiksmas = veiksmas.Replace("1", "+");
-            veiksmas = veiksmas.Replace("2", "-");
-            veiksmas = veiksmas.Replace("3", "*");
-            veiksmas = veiksmas.Replace("4", "/");
-            veiksmas = veiksmas.Replace("5", "^2");
-            veiksmas = veiksmas.Replace("6", "^3");
-        }
-
-
-            public static bool ArSvekiejiSkaiciai(string a, string b) => int.TryParse(a, out _) && int.TryParse(b, out _);
-            public static bool ArSkaiciai(string a, string b) => double.TryParse(a, out _) && double.TryParse(b, out _);
-            public static bool ArNaujasVeiksmas(string veiksmas) => veiksmas == "^2" || veiksmas == "^3";
-
-            public static double Suma(double a, double b) => a + b;
-            public static double Atimtis(double a, double b) => a - b;
-            public static double Daugyba(double a, double b) => a * b;
-            public static double Dalyba(double a, double b) => a / b;
-            public static double Kvadratu(double a) => a * a;
-            public static double Kubu(double a) => a * a * a;
-
-            public static double? Skaiciuotuvas(double a, double b, string veiksmas)
+            for (int i = 0; i <= 10; i+=3)
             {
-                switch (veiksmas) //state machine
-                {
-                    case "+":
-                        return Suma(a, b);
-                    case "-":
-                        return Atimtis(a, b);
-                    case "*":
-                        return Daugyba(a, b);
-                    case "/":
-                        return Dalyba(a, b);
-                    case "^2":
-                        return Kvadratu(a);
-                    case "^3":
-                        return Kubu(a);
-                    default:
-                        return null;
-                }
+                Console.WriteLine(" reiksme i : {0}", i);
             }
 
 
+            //for (char c = "a"; c >= "z"; c++)
+            //{
+            //    Console.WriteLine(" reiksme i : {0}", c);
 
+            //}
 
-        public static double? Skaiciuotuvas(int a, int b, string veiksmas)
-        {
-            switch (veiksmas) //state machine
+            int pradinisSKaicius = 30;
+            int zmoniuSkaicius = 0;
+            //while (zmoniuSkaicius > 10)
+            //{
+                
+            //    Console.WriteLine(pradinisSKaicius);
+            //}
+
+            do
             {
-                case "+":
-                    return Suma(a, b);
-                case "-":
-                    return Atimtis(a, b);
-                case "*":
-                    return Daugyba(a, b);
-                case "/":
-                    return Dalyba(a, b);
-                default:
-                    return null;
-            }
+               // Console.WriteLine("iveskite sk");
+                zmoniuSkaicius  = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine(pradinisSKaicius);
+            } while (zmoniuSkaicius > 10);
+
+
+
+
         }
 
-        public static int Suma(int a, int b) => a + b;
-        public static int Atimtis(int a, int b) => a - b;
-        public static int Daugyba(int a, int b) => a * b;
-        public static double Dalyba(int a, int b) => (double)a / b;
 
 
 
---
+++ 2 Lectures/P015 WhileDoCiklai/Program.cs
@@ -0,0 +1,194 @@
+﻿namespace P015_WhileDoCiklai
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, While/Do-While Ciklai!");
+            // WhileCikloPavyzdys();
+            // WhileCikloZaidejoPavyzdys();
+            // DoWhilePavyzdys();
+            // PirmoSprendimoApskaiciavimas();
+            // AntrasUzdavinys();
+            // TreciasUzdavinys();
+             MathRandomPavyzdys();
+            // SestaUzduotis();
+          
+
+        }
+
+        public static void WhileCikloPavyzdys() // 
+        {
+            int skaicius = 1;
+            while (skaicius <= 10)
+            {
+                Console.WriteLine(skaicius);
+                skaicius++;
+            }
+        }
+
+        public static void WhileCikloZaidejoPavyzdys()  // tol kol nepatenkina salygos tol kartojamas
+        {
+            int zaidejuSkaicius = 0;
+            while (zaidejuSkaicius < 1 || zaidejuSkaicius > 10)
+            {
+                Console.WriteLine("Kiek zaideju zais zaidima");
+                zaidejuSkaicius = Convert.ToInt32(Console.ReadLine());
+            }
+        }
+        public static void DoWhilePavyzdys()
+        {
+            int zaidejuSkaicius = 9;
+            do
+            {
+                Console.WriteLine("Kiek zaideju zais zaidima");
+                zaidejuSkaicius = Convert.ToInt32(Console.ReadLine());
+
+            } while (zaidejuSkaicius < 1 || zaidejuSkaicius > 10);
+        }
+
+
+        /* 1 uzdavinis
+
+        1.Paprašyti vartotojo įvesti bet kokį skaičių.     
+        * Išvesti skaičių sumą nuo 1 iki įvesto skaičiaus.       
+        */
+
+        public static void PirmoSprendimoApskaiciavimas()
+        {
+            int i = 0;
+            int suma = 0;
+          
+            Console.WriteLine("iveskite skaiciu sumai isgauti:");
+            i = Convert.ToInt32(Console.ReadLine());
+
+            while (i > 0)
+            {
+                Console.WriteLine($" skaiciuojama Suma: {suma}");
+                suma += i;
+                Console.WriteLine($" i: {suma}\n");
+                i--;
+            }
+            Console.WriteLine($"--------------");
+            Console.WriteLine($" Suma: {suma}");
+        }
+
+        /* 2 uzdavinys
+          Paprašyti vartotojo įvesti bet kokį skaičių.          
+          Išvesti visus lyginius skaičius nuo 0 iki pasirinkto skaičiaus,          
+          vienoje eilutėje per kablelį. Pvz.: 0, 2, 4, 6.....         
+         */
+
+        public static void AntrasUzdavinys()
+        {
+            int i = 0;
+            int j = 0;
+            Console.WriteLine("iveskite skaiciu sumai isgauti:");
+            i = Convert.ToInt32(Console.ReadLine());
+            while (j <= i)
+            {
+               if(j%2 == 0)
+                {
+                    Console.WriteLine(j);
+                }
+                j++;           
+            }
+        }
+
+        /*  3 uzdavinys        
+           Parasyti programa kuri apskaiciuoja visu ivestu skaiciu suma,          
+           kurie buvo ivesti iki ivesto neigiamo skaiciaus         
+            PVZ         
+            1,23,4,5,7,8,-1        
+          */
+
+        public static void TreciasUzdavinys()
+        {
+            int suma = 0;
+            int ivestis = 0;
+            do
+            {
+                suma += ivestis;
+                Console.WriteLine("iveskite skaiciu sumai isgauti:");
+                ivestis = Convert.ToInt32(Console.ReadLine());
+
+            } while (ivestis > 0);
+            Console.WriteLine($"suma : {suma}");
+        }
+
+
+
+        /* penkta uzduotis
+        5. Parasykite programa, kuri paklaustu naudotojo “skaicius ar herbas” ir naudotojas
+            galetu zaisti iki kol pasieke arba 10 pergaliu arba 10 pralaimejimu */
+        public static void MathRandomPavyzdys()
+        {
+            Random randomObjektas = new Random();
+            int ismestaMoneta = randomObjektas.Next(1,2);
+            int monetosMetimas = 0; 
+
+            while(monetosMetimas < 10)
+            {
+                monetosMetimas++;
+                Console.WriteLine(ismestaMoneta);
+                Console.WriteLine(randomObjektas.Next(1, 2));
+            }
+           
+
+
+        }
+
+
+
+
+        /*   6 uzduoties salyga     
+        6. Parasykite slaptazodzio ivedimo simuliacija. Pirma kompiuteris paprasys, kad nustatytumete slaptazodi 
+         tada prasys naudotojo pakartoti slaptazodi. Bet koks neteisingas ivedimas grazina “Slaptazodis neteisingas. 
+          Bandykite dar karta”.        
+         Kada slaptazodis atspejamas turi buti isvedamas tekstas “Sveikinam! Prisijungete!”.   
+         BONUS TASKAI: Padarykite taip, kad po 3 neteisingai ivestu slaptazodzio kartu programa ismestu teksta 
+         “Jus uzblokuotas” ir iseitu is ciklo.      
+        */
+
+        public static void SestaUzduotis()
+        {
+            string code, userCode;
+            int bandymai = 0;
+            int prisijungimoStatusas = 0;
+
+            Console.WriteLine("Iveskite nauja Pin: ");
+            userCode = Console.ReadLine();
+
+            do
+            {
+                Console.WriteLine("Iveskite Pin: ");
+                code = Console.ReadLine();
+                Console.WriteLine("Bandymas {0} is 3 ", bandymai + 1);
+
+                if (code == userCode)
+                {
+                    prisijungimoStatusas = 1;
+                    bandymai = 3;
+                }
+
+                else
+                {
+                    prisijungimoStatusas = 0;
+                    bandymai++;
+                }
+            }
+            while ((code != userCode) && (bandymai != 3));
+            if (prisijungimoStatusas == 0)
+            {
+                Console.WriteLine("Buvo 3 bandymai. Blokuotas!");
+            }
+            else if (prisijungimoStatusas == 1)
+            {
+                Console.WriteLine("Prisijungta sekmingai!");
+            }
+
+
+
+        }
+    }
+}
