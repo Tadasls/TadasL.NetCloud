6dff841bd6e4d4796009b1f7ea330e8b7d640352
Tadas Laurinaitis <tadas.laurinaitis@gmail.com>
2022-07-13 22:46:11 +0300

    2 form

--- 2 Lectures/P017_ForUzduotys/Program.cs
+++ 2 Lectures/P017_ForUzduotys/Program.cs
@@ -7,23 +7,256 @@ namespace P017_ForUzduotys
         {
             Console.WriteLine("Hello, For uzduotys!");
 
-            //ReadIntNumber();
+            // ReadIntNumber();
             // IntegerToBinary(233);
             // PakeltiLaipsniu(5, 2);
+            // SkaiciuTrikampis();
+            // SkaiciuPiramide();
+            // SkaiciuEile();
 
-            SkaiciuTrikampis();
+            // PenktaUzduotis();
+            // PirmasUzdavinysSkaiciuEile();
+             DaugybosLentele();
+            // KetvirtaUzduotis();
+            // Skaiciuotuvas();
 
-            Console.WriteLine("iveskite skaiciu");
-            var skaicius = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("iveskite laipsny");
-            var laispnis = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("rezultatas " + PakeltiLaipsniu(skaicius, laispnis));
+
+            //Console.WriteLine("iveskite skaiciu");
+            //var skaicius = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("iveskite laipsny");
+            //var laispnis = Convert.ToInt32(Console.ReadLine());
+
+            //Console.WriteLine("rezultatas " + PakeltiLaipsniu(skaicius, laispnis));
 
 
         }
 
 
+        /*  3. Sukurti skaiciuotuva.
+                
+           Pasirinkus 2 programa turetu paprasyti ivesti kokia operacija turetu buti atliekama ir paprasyti TIK SEKANCIO SKAITMENS.
+        Pasirinkus 3 programa turetu issijungti.Visa kita turetu veikti tokiu pat budu.
+           BONUS2: Parasykite operacijoms validacijas pries ivestus neteisingus skaicius.
+        Pvz: dalyba is nulio, neteisingu ivesciu prevencija pvz kada tikimasi gauti skaiciu, bet gaunamas char arba string.
+           BONUS3: Parasyti unit testus uztikrinant operaciju veikima
+           
+            */
+
+        public static int Skaiciuotuvas()
+        {
+            int sk1 = 0;
+            int sk2 = 0;
+
+
+            Console.WriteLine(" 1. Nauja operacija \n 2. Testi su rezultatu \n 3. Iseiti. ");
+            string mainMeniu = Convert.ToString(Console.ReadLine());
+            switch (mainMeniu)
+            {
+                case "1":
+                    // 1. Nauja operacija
+
+                    break;
+                case "2":
+                    //  2. Testi su rezultatu
+
+                    break;
+                case "3":
+                    Console.WriteLine("Exit");
+                    System.Environment.Exit(-1);
+                    break;
+            }
+
+            Console.WriteLine($" 1. Sudetis \n 2. Atimtis \n 3. Daugyba \n 4. Dalyba");
+            string pirmasSubmeniu = Convert.ToString(Console.ReadLine());
+            switch (pirmasSubmeniu)
+            {
+                case "1":
+                    Console.WriteLine("Iveskite 1 ir 2 skaiciu");
+                    sk1 = Convert.ToInt32(Console.ReadLine());
+                    sk2 = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine(SudetiSkaicius(sk1, sk2));
+                    Skaiciuotuvas();
+                    break;
+                case "2":
+                    Console.WriteLine("Iveskite 1 ir 2 skaiciu");
+                    sk1 = Convert.ToInt32(Console.ReadLine());
+                    sk2 = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine(AtimtiSkaicius(sk1, sk2));
+                    Skaiciuotuvas();
+                    break;
+                case "3":
+                    Console.WriteLine("Iveskite 1 ir 2 skaiciu");
+                    sk1 = Convert.ToInt32(Console.ReadLine());
+                    sk2 = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine(DaugintiSkaicius(sk1, sk2));
+                    Skaiciuotuvas();
+                    break;
+                case "4":
+                    Console.WriteLine("Iveskite 1 ir 2 skaiciu");
+                    sk1 = Convert.ToInt32(Console.ReadLine());
+                    sk2 = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine(DalintiSkaicius(sk1, sk2));
+                    Skaiciuotuvas();
+                    break;
+                case "5":
+                    Console.WriteLine("Iveskite 1 skaiciu");
+                    sk1 = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine(LaipsniuKelimoSkaicius(sk1));
+                    Skaiciuotuvas();
+                    break;
+                case "6":
+                    Console.WriteLine("Iveskite 1 skaiciu");
+                    sk1 = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine(SakniesTraukimoSkaicius(sk1));
+                    Skaiciuotuvas();
+                    break;
+
+                default:
+                    break;
+            }
+
+
+
+
+
+            return 1;
+        }
+
+       
+
+        public static int SudetiSkaicius(int sk1, int sk2)
+        {
+            return sk1 + sk2;
+        }
+        public static int AtimtiSkaicius(int sk1, int sk2)
+        {
+            return sk1 - sk2;
+        }
+        public static int DaugintiSkaicius(int sk1, int sk2)
+        {
+            return sk1 * sk2;
+        }
+        public static double DalintiSkaicius(int sk1, int sk2)
+        {
+            return (double)sk1 / sk2;
+        }
+        public static int LaipsniuKelimoSkaicius(int sk1)
+        {
+                int valSqr = 0;
+                for (int i = 0, j = 1; i < sk1; i++, j += 2)
+                    valSqr += j;
+                return valSqr;
+        }
+        public static double SakniesTraukimoSkaicius(int sk1)
+        {    
+            double root = 1;
+            int i = 0;
+          
+            while (true)
+            {
+                i = i + 1;
+                root = (sk1 / root + root) / 2;
+                if (i == sk1 + 1) { break; }
+            }
+            Console.WriteLine("Square root:{0}", root);
+            return root;
+
+        }
+
+
+        /*  NAUJAS 1
+          ----------------------------
+          Parašykite metodą SkaiciuEile kuri išvestu vienoje eilutėje skaičių grupes tokiu principu: -> 1 -> 11 -> 111 -> 1111 -> 11111 -> ....... 
+          programa turi paprašyti nurodyti skaičių ir grupių kiekį
+          naudokite for ir StringBuilder
+        */
+
+        public static void PirmasUzdavinysSkaiciuEile()
+        {
+            int input1 = 10;
+            int input2 = 10;
+            var sb7 = new StringBuilder();
+
+            for (int i = 0; i < input2; i++)
+            {
+                Console.Write("->");
+                sb7.Append(input1);
+                Console.Write(sb7);
+
+            }
+        }
+
+        /*        2. Sukurkite programa, kuri paprasytu naudotojo ivesti skaiciu.Ivedus skaiciu turetu atspausdinti to skaiciaus daugybos lentele.Pvz
+        15 X 1 = 15                                                                                                   
+        15 X 2 = 30                                                                                                   
+        15 X 3 = 45                                                                                                   
+        15 X 4 = 60                                                                                                   
+        15 X 5 = 75                                                                                                   
+        15 X 6 = 90                                                                                                   
+        15 X 7 = 105                                                                                                  
+        15 X 8 = 120                                                                                                  
+        15 X 9 = 135                                                                                                  
+        15 X 10 = 150
+        */
+
+        public static void DaugybosLentele()
+        {
+            Console.WriteLine("Iveskite skaiciu");
+            int skaicius = Convert.ToInt32(Console.ReadLine());
+            var sb = new StringBuilder();
+
+            for (int i = 1; i <= 10; i++)
+            {
+                // sb.Append(skaicius).Append(" X ").Append(i).Append(" = ").Append(skaicius * i);
+                
+                sb.Append($"{skaicius} X {i} = {skaicius * i}").AppendLine();
+
+                Console.WriteLine(sb);
+                sb.Clear();
+            }
+        }
+        
+
+
+
+        /* DidejanciuSkaiciuPiramide
+    Sukurti metodą DidejanciuSkaiciuPiramide, kuri paprašo vartotojo įvesti skaičių nuo 1 iki 9 
+    (jeigu įveda netinkamą skaičių prašo įvesti vėl, kol įves tinkamą )). 
+    Metodas turi grąžinti atitinkamą lygiašonį trikampį (ivestas skaičius 4).
+    1
+    22
+    333
+    4444
+    333
+    22
+    1
+    */
+
+        //  DidejanciuSkaiciuPiramide 5555 uzdavinio sprendimas kolegos
+
+
+        public static int SkaiciausTikrinimas(string? txt) => int.TryParse(txt, out int num) == false ? 0 : num;
+        public static void PenktaUzduotis()
+        {
+            int skaicius = 0;
+            while (skaicius < 1 || skaicius > 9)
+            {
+                Console.Write("Iveskite skaiciu: ");
+                skaicius = SkaiciausTikrinimas(Console.ReadLine());
+            }
+            StringBuilder sb = new StringBuilder();
+            for (int i = 1; i <= skaicius; i++)
+            {
+                sb.Append(skaicius);
+                Console.WriteLine(sb.ToString());
+            }
+            for (int i = skaicius; i >= 1; i--)
+            {
+                sb.Remove(0, 1);
+                Console.WriteLine(sb.ToString());
+            }
+        }
 
 
         /* 77 salyga
@@ -45,21 +278,43 @@ namespace P017_ForUzduotys
         7
          */
 
-
-        /* DidejanciuSkaiciuPiramide
-Sukurti metodą DidejanciuSkaiciuPiramide, kuri paprašo vartotojo įvesti skaičių nuo 1 iki 9 
-(jeigu įveda netinkamą skaičių prašo įvesti vėl, kol įves tinkamą )). 
-Metodas turi grąžinti atitinkamą lygiašonį trikampį (ivestas skaičius 4).
-1
-22
-333
-4444
-333
-22
-1
-*/
+        public static string SkaiciuPiramide()
+        {
+            Console.WriteLine("iveskite nuo 1 iki 9");
 
 
+            int vartSkaicius = Convert.ToInt32(Console.ReadLine());
+            bool arSkaiciusGeras = false;
+           
+                while (vartSkaicius >= 1 && vartSkaicius <= 9)
+            {
+                StringBuilder sb3 = new StringBuilder();
+
+                for (int i = 1; i < vartSkaicius; i++)
+                {
+                    for (int j = 1; j < i + 1; j++)
+                    {
+                        sb3.Append(vartSkaicius);
+                    }
+                    sb3.AppendLine();
+                }
+
+                for (int i = vartSkaicius; i >= 0; i--)
+                {
+                    for (int j = 1; j < i-1; j++)
+                    {
+                        sb3.Append(vartSkaicius);
+                    }
+                    sb3.AppendLine();
+                }
+
+                Console.WriteLine(sb3);
+                 return sb3.ToString();
+
+            }
+            Console.WriteLine("reikia gero skaiciaus");
+            return " netinkamas skaicius";
+        }
 
         /* 55 salyga
      Sukurti metodą SkaiciuTrikampis, kuri paprašo vartotojo įvesti skaičių nuo 1 iki 9
@@ -72,36 +327,108 @@ Metodas turi grąžinti atitinkamą lygiašonį trikampį (ivestas skaičius 4).
   55555
    */
 
-        public static string SkaiciuTrikampis()
+
+
+        private static void KetvirtaUzduotis()
         {
-            Console.WriteLine("iveskite nuo 1 iki 9");
-            int skaiciusVartotojo = Convert.ToInt32(Console.ReadLine());
-
-            while (skaiciusVartotojo >= 1 && skaiciusVartotojo <= 9)
+            int skaicius = 0;
+            while (skaicius < 1 || skaicius > 9)
             {
-
-                StringBuilder sb1 = new StringBuilder();
-
-                for (int i = 1; i < skaiciusVartotojo; i++)
-                {
-
-                    for (int j = 1; j < i + 1; j++)
-                    {
-                        sb1.Append(i);
-
-
-                    }
-
-                }
-                Console.WriteLine(sb1);
-                return sb1.ToString();
-
+                Console.Write("Iveskite skaiciu: ");
+                skaicius = SkaiciausTikrinimasNuo1Iki9(Console.ReadLine());
             }
 
-            return "nesuveike";
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i <= skaicius; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    sb.Append(Convert.ToString(skaicius));
+                }
+                sb.AppendLine("");
+            }
+            Console.WriteLine(sb.ToString());
 
         }
+        private static int SkaiciausTikrinimasNuo1Iki9(string? txt)
+        {
+            if (int.TryParse(txt, out int num) == false)
+                return 0;
+            else
+                return num;
+        }
 
+
+        //BANDYMAS PER PASKAITA REFACTORINTI
+        //
+        //public static void SKaiciuuTrikmapis()
+        //{
+        //    var input1 = string.Empty;
+
+        //    var skaicius = 0;
+        //    var arValidu = ArValidu(out input1, out skaicius);
+
+        //}
+        //public static string SudeliotiSakini(int skaicius)
+        //{
+        //    var builder = new StringBuilder();
+        //    for (int i =0; i< skaicius; i++)
+        //    {
+
+        //    }
+        //    return builder;
+        //}
+
+        //public static bool ArValidu(out string input1, out int skaicius)
+        //{
+        //    while (true)
+        //    {
+        //        input1 = Console.ReadLine();
+        //        var arTeiginga = int.TryParse(input1, out skaicius);
+
+
+        //        if (skaicius <= 9 && skaicius >= 1)
+        //        {
+        //          break;
+        //        }
+
+        //    }
+        //    return builder;
+        //}
+
+
+        //public static string SkaiciuTrikampis()
+        //{
+        //    Console.WriteLine("iveskite nuo 1 iki 9");
+        //    bool arSkaiciusTeisingas = false;
+        //    int skaiciusVartotojo = Convert.ToInt32(Console.ReadLine());
+
+        //    while (!arSkaiciusTeisingas)
+        //    {
+        //        skaiciusVartotojo = Console.ReadLine();
+        //        arSkaiciusTeisingas = int.TryParse(skaiciusVartotojo, out skaicius6);
+        //        if (!arSkaiciusTeisingas) Console.WriteLine("Ivestas neteisingas skaicius, bandykite dar");
+        //    }
+
+
+        //    StringBuilder sb1 = new StringBuilder();
+
+        //    for (int i = 1; i < skaiciusVartotojo; i++)
+        //    {
+
+        //        for (int j = 1; j < i + 1; j++)
+        //        {
+        //            sb1.Append(skaiciusVartotojo);
+        //        }
+        //        sb1.AppendLine();
+
+
+        //        Console.WriteLine(sb1);
+
+        //    }
+
+        //    return sb1.ToString();
+        //}
 
         /* laipsniu kelimo metodas salyga
                 Sukurti metodą PakeltiLaipsniu , kuris duotą skaičių pakelia nurodytu
@@ -169,6 +496,7 @@ neįveda tinkamo skaičiaus metodas turi vis prašyti įvesti.
             string skaiciusStr = string.Empty;
             bool arSkaiciusTeisingas = false;
             int skaicius = 0;
+
             while (!arSkaiciusTeisingas)
             {
                 skaiciusStr = Console.ReadLine();
