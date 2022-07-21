eaff88252a81ae46b845155e7945b375a7b43640
Tadas Laurinaitis <tadas.laurinaitis@gmail.com>
2022-07-21 22:10:26 +0300

    321

---
+++ 2 Lectures/P011_Metodu_Testai/P023DictionariesTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metodu_Testai
+{
+    [TestClass]
+    public class P023DictionariesTest
+    {
+
+        //[TestMethod]
+        //public void ApskaiciuotiVidurki()
+        //{
+        //    var fake = new List<double> { 1, 5, 8, 9, 8, 5 };
+        //    int expected = 6;
+        //    var actual = P023_Dictionaries.Program.ApskaiciuotiVidurki(fake);
+        //    Assert.AreEqual(expected, actual);
+        //}
+
+
+
+
+    }
+}
