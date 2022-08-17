195bef11c4b05697b5c8b1cf2596ccf0ac21fa9a
Tadas Laurinaitis <tadas.laurinaitis@gmail.com>
2022-08-17 22:38:14 +0300

    ata

---
+++ OOP/P035_DataReading/P035_DataReading.Domain/Enums/ESocietySortBy.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P035_DataReading.Domain.Enums
+{
+    public enum ESocietySortBy { FirstName, LastName }
+}
