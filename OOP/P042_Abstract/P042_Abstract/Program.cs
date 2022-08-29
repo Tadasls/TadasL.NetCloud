6794b7f50ca76b17807aff6cf1f36097845cbd60
Tadas Laurinaitis <tadas.laurinaitis@gmail.com>
2022-08-29 18:42:06 +0300

    321

---
+++ OOP/P042_Abstract/P042_Abstract/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using P042_Abstract;
+
+Console.WriteLine("Hello, Abstract!");
+
+//SearchEngine searchEngine = new SearchEngine(); //taip negalima
+SearchEngine googleSearchEngine = new GoogleSearchEngine();
+SearchEngine duckDuckGoSearchEngine = new DuckDuckGoSearchEngine();
+
+string[] googleResults = googleSearchEngine.Search("aa");
+string[] duckDuckGoResults = duckDuckGoSearchEngine.Search("bb");
+Console.WriteLine($"googleResults = [{string.Join(", ", googleResults)}]");
+Console.WriteLine($"duckDuckGoResults = [{string.Join(", ", duckDuckGoResults)}]");
+
+string[] googleResults1 = googleSearchEngine.Result("");
+string[] duckDuckGoResults1 = duckDuckGoSearchEngine.Result("");
+Console.WriteLine($"googleResults1 = [{string.Join(", ", googleResults1)}]");
+Console.WriteLine($"duckDuckGoResults1= [{string.Join(", ", duckDuckGoResults1)}]");
