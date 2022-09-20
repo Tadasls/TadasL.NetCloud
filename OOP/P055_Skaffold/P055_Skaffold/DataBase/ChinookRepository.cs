721d499e9a6a22d95ec7daa54706d15f9be43d6b
Tadas Laurinaitis <tadas.laurinaitis@gmail.com>
2022-09-20 22:10:42 +0300

    gogo

--- OOP/P055_Skaffold/P055_Skaffold/DataBase/ChinookRepository.cs
+++ OOP/P055_Skaffold/P055_Skaffold/DataBase/ChinookRepository.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using P055_Skaffold.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,58 @@ using System.Threading.Tasks;
 
 namespace P055_Skaffold.DataBase
 {
-    internal class ChinookRepository
+    public class ChinookRepository
     {
+
+        
+        private readonly ChinookContext _context;
+
+        public ChinookRepository(ChinookContext context)
+        {
+            _context = context;
+        }
+
+       
+      // 2. Prašykite metodą, kuris pagal pateiktą 'Country' grąžintų visus 'customers' iš tos šalies (vardą, kliento id ir šalies pavadinimą)
+
+        public IEnumerable<dynamic> GetCustomer(string country)
+        {
+            using (var ctx = new ChinookContext())
+            {
+                var res = ctx.Customers.Where(x => x.Country == country)
+                    .Select(c => new
+                {
+                    Vardas = c.FirstName,
+                    KlientoId = c.CustomerId,
+                    SaliesPavadinimas = c.Country,
+                });
+                return res;
+            }
+
+        }
+
+
+
+
+        // 3. Prašykite metodą, kuris pagal pateiktą kliento 'Country' grąžintų visus 'invoice' iš tos šalies
+        // (kliento pilnas vardas, sąskaitos id, sąskaitos data, sąskaitos šalis)
+
+        public List<Invoice> GetInvoices(string country)
+        {
+            using (var context = new ChinookContext())
+            {
+                var invoisuSarasas = context.Invoices.Include(x=> x.Customer.Country == country);
+                return invoisuSarasas.ToList();
+            }
+
+        }
+
+
+
+
+
+
+
+
     }
 }
