fa434d8e969ce294de712ea18c693f3beb9af9a7
Tadas Laurinaitis <Tadas.Laurinaitis@gmail.com>
2022-09-25 10:43:23 +0300

    123654

---
+++ DB/P052_CodeFirstDB/P052_CodeFirstSqliteDb.Domains/Modeliai/Animal.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P052_CodeFirstSqliteDb.Domain.Modeliai
+{
+    public class Animal
+    {
+        public Animal()
+        {
+
+        }
+
+        public Animal(string name, string type, DateTime birthDate)
+        {
+            Name = name;
+            Type = type;
+            BirthDate = birthDate;
+        }
+
+        [Key]
+        public int AnimalId { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public DateTime BirthDate { get; set; }
+
+
+
+
+    }
+}
