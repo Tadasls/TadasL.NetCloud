18816acd52cc40c6cfa65f61940a8a3c512a98cd
Tadas Laurinaitis <Tadas.Laurinaitis@gmail.com>
2022-10-23 21:05:02 +0300

    homeworks

--- HTML/P10_Arrays/HomeWorks/js/app.js
+++ HTML/P10_Arrays/HomeWorks/js/app.js
@@ -1,28 +1,73 @@
-function helloWorld() {
 
-    let worldText = 'World';
-    console.log("Hello World!");
-    console.log(`Hello ${worldText}!`);
-}
+// IsPassWordValid();
 
-helloWorld();
+function IsPassWordValid(userName, passWord){
 
-
-
-function throwDie() {
-    let roll = Math.floor(Math.random() * 6) + 1;
-    console.log(`Die was rolled: ${roll}`);
-}
-throwDie();
-throwDie();
-throwDie();
-
-
-let usserName = prompt("Įveskite UsserName:");
-let password = prompt("Įveskite Password:");
-
-let slaptazodzioIlgis = password.length;
-if (slaptazodzioIlgis === 8) { 
+    userName = prompt("Įveskite UsserName:");
+    passWord = prompt("Įveskite Password:");
+           
+    let slaptazodzioIlgis = passWord.length;
+    let arSutapimas = passWord == userName;
+    let passA = passWord.trim();
+    let tarpuSkaicius = passWord.length - passA.length;
     
+    slaptazodzioIlgis !== 8 
+    ? alert("Slaptazodis per Trumpas/PerIlgas")
+    : arSutapimas === true 
+    ? alert("Slaptazodis negali buti UsserName")
+    : tarpuSkaicius >= 1
+    ? alert("Slaptazodyje negali buti tusti tarpai")
+    : alert("Sveikiname Prisijungus");
+        
 }
-console.log(`slaptazodzio ilgumas yra  ${slaptazodzioIlgis}`);
+
+let array = [19,22,56.80,12,51, 100];
+let vidurkioSuma = 0;
+function GetAverageOfArray(array){
+   
+
+
+    for(let skaicius of array) {
+       vidurkioSuma = skaicius + vidurkioSuma;
+    }
+
+ let vidurkis = vidurkioSuma / array.length;
+ console.log(vidurkis.toFixed(2));
+}
+GetAverageOfArray(array);
+
+
+
+let show = function() {
+    console.log(`Anononymous function`);
+};
+
+let add = function(a,b) {
+    return a+b;
+};
+
+show();
+console.log(add(2,2));
+
+
+// let param1 = 5;
+// let param2 = 7;
+// const getData = console.log(param1,param2);
+
+console.log(`tadas`);
+
+
+let cardType = ['Clubs', 'Spades', 'Hearts', 'Diamonds'];
+let cardValue = ['A','2','3','4','5','6','7','8','9','10','J','Q','K'];
+
+
+function pickOne(cardType,cardValue) {
+    
+    let randomCardType = cardType[Math.floor(Math.random() * cardType.length)];
+    let randomCardValue = cardValue[Math.floor(Math.random() * cardValue.length)];
+    console.log(randomCardType);
+    let vaizdas = randomCardValue.replace('A', 'Ace').replace('J', 'Joker').replace('Q', 'Queen').replace('K', 'King');
+    console.log(`You drew ${vaizdas} of ${randomCardType}   `);
+}        
+
+pickOne(cardType,cardValue);
