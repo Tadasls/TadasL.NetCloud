a0badba50d7cfa83314b230d4acc15c087d9f936
Tadas Laurinaitis <Tadas.Laurinaitis@gmail.com>
2022-11-16 17:44:51 +0200

    852

---
+++ WEB API/PirmaPaskaita/naujas/Book.cs
@@ -0,0 +1,10 @@
+﻿namespace naujas
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Pavadinimas { get; set; }
+        public string Autorius { get; set; }
+        public int leidybosMetai { get; set; }
+    }
+}
