5ae9b1bf488ab6ab0c11a7f51fbc3042bd3eae81
Tadas Laurinaitis <Tadas.Laurinaitis@gmail.com>
2022-12-28 12:09:54 +0200

    654

---
+++ WEB API/WebAppMSSQLTests/Repository/UserRepositoryTests.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WebAppMSSQL.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebAppMSSQL.Models.DTO;
+using Microsoft.Extensions.Logging;
+using Moq;
+using WebAppMSSQL.Controllers;
+using WebAppMSSQL.Repository.IRepository;
+using WebAppMSSQL.Services.IServices;
+using WebAppMSSQL.Data;
+using WebAppMSSQL.Services;
+
+namespace WebAppMSSQL.Repository.Tests
+{
+    [TestClass()]
+    public class UserRepositoryTests
+    {
+        //[TestMethod()]
+        //public void LoginAsyncTest()
+        //{
+        //    var loginRequest = new LoginRequest
+        //    {
+        //        Username = "testuser",
+        //        Password = "correctpassword"
+        //    };
+
+
+            
+            
+        //    //var membersService = new Mock<IMembershipService>();
+        //    //var stockService = new Mock<IStockService>();
+        //    //var userRepo = new Mock<IUserRepository>();
+        //    //var userRepo = new KnygynasContext _db;
+        //    //var userRepo = new IPasswordService _passwordService;
+        //    //var jwtService  = new Mock<IJwtService>();
+        //    //var membersService = new IMembershipService _membersService;
+
+
+
+
+        //    var userService = new UserRepository(membersService.Object, stockService.Object);
+
+        //    // Act
+        //    var loginResponse =  userService.LoginAsync(loginRequest);
+
+        //    // Assert
+        //    Assert.IsNotNull(loginResponse);
+        //}
+    }
+}
